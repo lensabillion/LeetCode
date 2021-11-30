2648471c0b5966a6339a43957b056953e4dad688
lensabillion <billionlensa2019@gmail.com>
2021-11-30 10:53:03 +0300

    cheapest flights

--- djikstra/Cheapest Flights Within K Stops.py
+++ djikstra/Cheapest Flights Within K Stops.py
@@ -2,30 +2,33 @@ import heapq
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         graph = {}
-        visited = set()
+        direction = {}
         graph = [[] for _ in range(n)]
         
         for u, v, c in flights:
             graph[u].append([v, c])
-        
-        _maxcost = float('inf')
-        q = [(0, -1, src)]
+        for i in range(n):
+            direction[i] = [float('inf'), -1]
+        direction[src][0] = 0
+     
+       
+        q = [(-1, 0, src)]
         while q:
             
-            if len(visited) >= n:
-                break
-            curr_cost, stop, curr_dest = heapq.heappop(q)
-           
-            if curr_dest == dst and stop <= k:
-                _maxcost = min(_maxcost, curr_cost)
-            
+            stop, curr_cost, curr_dest = heapq.heappop(q)
+            if stop > direction[curr_dest][1]:
+                continue
             for next_dest, cost in graph[curr_dest]:
                 new_cost = curr_cost + cost
                 s = stop + 1
-                if next_dest not in visited:
-                    heapq.heappush(q,(new_cost, s, next_dest))
-            visited.add(curr_dest)
-        
-        if _maxcost == float('inf'):            
+                if new_cost < direction[next_dest][0] and k >= direction[next_dest][1] and s <= k:
+                   
+                    direction[next_dest][0] = new_cost
+                    direction[next_dest][1] = s
+                    heapq.heappush(q, (s, new_cost, next_dest))
+          
+        if direction[dst][0] == float('inf'):
             return -1
-        return _maxcost
+        return direction[dst][0]
+        
+        
